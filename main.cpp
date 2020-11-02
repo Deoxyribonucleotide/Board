ef602acf77f896738048fb7c1aba6d053b0da513
Nick <nickrcpj@gmail.com>
2020-11-02 15:29:08 +0300

    fixes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,56 @@
 #include <SFML/Graphics.hpp>
-#include<iostream>
+
+using namespace sf;
+using namespace std;
 
 int main()
 {
-    
+    RenderWindow window(VideoMode(1000, 600), "Board");
+    CircleShape shape(8);
+    shape.setFillColor(Color::Green);
+    RenderTexture texture;
+    texture.create(1000, 600);
+    texture.clear();
+    Sprite spr(texture.getTexture());
+
+    Vector2i pixelPos = Mouse::getPosition(window);
+    Vector2f pos = window.mapPixelToCoords(pixelPos);
+
+    int x = 0, y = 0;
+    bool draw = true;
+
+    while (window.isOpen())
+    {
+        Vector2i pixelPos = Mouse::getPosition(window);
+        Vector2f pos = window.mapPixelToCoords(pixelPos);
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+            if (Keyboard::isKeyPressed(Keyboard::B))
+                shape.setFillColor(Color::Blue);
+
+            if (Keyboard::isKeyPressed(Keyboard::Q))
+                texture.clear();
+            
+            if (event.type == Event::MouseButtonPressed)
+               if (event.key.code == Mouse::Right)
+                    draw = !draw;
+                
+            if (draw)
+            {
+                x = pos.x;
+                y = pos.y;
+                shape.setPosition(x, 600 -y);
+                texture.draw(shape); 
+            }  
+        }
+        window.clear();
+        window.draw(shape);
+        window.draw(spr);
+        window.display();
+    }
+
     return 0;
 }
